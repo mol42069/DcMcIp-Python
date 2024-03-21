d229cd37177bef3d3c40b80e71815a68ac456588
Moritz Weber <morationisch@gmail.com>
2024-03-21 10:57:41 +0100

    fixed it so it only sends into the right channel, and made it easier to set all the variables.

--- main.py
+++ main.py
@@ -6,27 +6,19 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix="$", intents = intents)
+bot = commands.Bot(command_prefix="$", intents=intents)
 msg = None
 m = None
 running = False
+McRunning = False
 
+user_role = 'YOUR USER-ROLE HERE'   # must be in lowercase
+channel_name = 'THE DISCORD-CHANNEL NAME HERE'  # must be identical to the discord-channel-name
+discord_Token = 'YOUR DISCORD TOKEN HERE'
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-
-#@client.event
-#async def on_message(message):
-#    if message.author == client.user:
-#        return
-#
-#    if message.content == 'ip' or message.content == 'Ip' or message.content == 'IP' or message.content == 'iP'\
-#        or message.content == ' ip' or message.content == 'ip ':
-#        ip = str(fetchip() + ':25565')
-#        user = await client.fetch_user(message.author.id)
-#        await user.send(ip)
-#        await message.delete()
 
 @client.event
 async def on_message(message):
@@ -35,39 +27,29 @@ async def on_message(message):
         temp = True
         channel = message.channel
 
-        if message.content == '!startIPbot' and "besserer-mensch" in [y.name.lower() for y in message.author.roles]:
-            running = True
-            async for b in channel.history(limit=100):
-                if b.author == client.user:  # client.user or bot.user according to what you have named it
-                    await b.delete()
-            #msg = await channel.send('Send me that ✅ reaction, to get the Minecraft ip')
-            #await msg.add_reaction('✅')
-            await message.delete()
+        if channel.name != channel_name:
+            return
 
-        elif message.content == '!start' and "besserer-mensch" in [y.name.lower() for y in message.author.roles]:
-            running = True
-            #msg = await channel.send('Send me that ✅ reaction, to get the Minecraft ip')
-            await message.delete()
-            #await msg.add_reaction('✅')
+        # replace "discord-role" with the role you want to start or stop the Bot
 
-        elif message.content == '!stop' and "besserer-mensch" in [y.name.lower() for y in message.author.roles]:
+        if message.content == '!start' and user_role in [y.name.lower() for y in message.author.roles]:
+            running = True
+            # msg = await channel.send('Send me that ✅ reaction, to get the Minecraft ip')
+            await message.delete()
+            # await msg.add_reaction('✅')
+
+        elif message.content == '!stop' and user_role in [y.name.lower() for y in message.author.roles]:
             running = False
             if msg is not None:
                 await message.delete()
                 await msg.delete()
                 return
-                #quit(0)
 
-        elif message.content == '!McStart' and "besserer-mensch" in [y.name.lower() for y in message.author.roles]:
-            await message.delete()
-            os.system(
-                'x-terminal-emulator -e "$echo here-your-pw" | sudo -S java -Xms1G -Xmx12G -jar server.jar nogui')
 
         if running:
             if msg is not None:
                 temp = False
                 omsg = msg
-                # await msg.delete()
 
             msg = await channel.send('Send me that ✅ reaction, to get the Minecraft ip')
             await msg.add_reaction('▫️')
@@ -77,18 +59,20 @@ async def on_message(message):
                 await omsg.delete()
 
 
-
 @client.event
 async def on_reaction_add(reaction, user):
 
     global msg, m
+
+    # TODO: here we might want to check if the user clicking has specific
+
     if msg is not None and user != client.user and reaction.emoji == "✅":
 
         ip = str(fetchip() + ':25565')
 
-        #user = await client.fetch_user(reaction.author.id)
+        # user = await client.fetch_user(reaction.author.id)
         m = await user.send("...")
-        dmchannel =  m.channel # dm channel you want to clear
+        dmchannel = m.channel   # dm channel you want to clear
         async for message in dmchannel.history(limit=100):
             if message.author == client.user:  # client.user or bot.user according to what you have named it
                 await message.delete()
@@ -99,11 +83,9 @@ async def on_reaction_add(reaction, user):
         await msg.add_reaction('✅')
 
 
-
-
-
 def fetchip():
     r = requests.get( 'https://ipinfo.io/ip', auth=('user', 'pass'))
     return r.text
 
-client.run('HERE YOUR DISCORD TOKEN')
+
+client.run(discord_Token)
